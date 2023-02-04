92490642928655b16611fb7f650bc859b07e10d5
Arthur Zennig <arthur.zennig@gmail.com>
2023-02-04 18:16:20 -0300

    constants added

---
+++ Shared/Constants.py
@@ -0,0 +1,9 @@
+"""
+Important angles, their names and description in order to substitute magic numbers.
+"""
+
+
+class Angle:
+    ZENITH = 90  # 90 degrees azimuth has the name of zenith. It is when the celestial body is right above observer's
+    # head.
+    HORIZON = 0  # 0 degree azimuth is the horizon from observer.
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import numpy as np
-import cv2
+"""
+This script crops a source image based on azimuth (as degrees) and coordinates as hours.
+"""
 from skyfield import api, almanac
 from PIL import ImageDraw, Image, ImageFilter
+from Shared.Constants import Angle
 
-def crop_center(pil_img,x,y, crop_width, crop_height):
+
+def crop_center(pil_img, x, y, crop_width, crop_height):
     img_width, img_height = pil_img.size
-    left = int(x - 0.5*crop_width)
-    upper = int(y - 0.5*crop_height)
-    right = int(x + 0.5*crop_width)
-    bottom = int(y + 0.5*crop_height)
+    left = int(x - 0.5 * crop_width)
+    upper = int(y - 0.5 * crop_height)
+    right = int(x + 0.5 * crop_width)
+    bottom = int(y + 0.5 * crop_height)
     left = img_width if (left > img_width) else left
     left = 0 if (left < 0) else left
     upper = img_height if (upper > img_height) else upper
-    upper = 0 if (upper < 0 ) else upper
+    upper = 0 if (upper < 0) else upper
     right = 0 if right < 0 else right
     right = img_width if right > img_width else right
     bottom = 0 if bottom < 0 else bottom
@@ -32,51 +35,59 @@ def mask_circle_solid(pil_img, offset=0):
     result.putalpha(mask)
     return result
 
+
 '''
 retorna right ascension (em horas) e longitude from skymap that is right above the observers head.
 https://rhodesmill.org/skyfield/examples.html#when-is-a-body-or-fixed-coordinate-above-the-horizon
 '''
-def get_ra_dec_above_observer(year,month,day,hour,min,lat,lon):
+
+
+def get_ra_dec_above_observer(year, month, day, hour, min, lat, lon):
     ts = api.load.timescale()
     t = ts.utc(year=year, month=month, day=day, hour=hour, minute=min)
     geographic = api.wgs84.latlon(latitude_degrees=lat, longitude_degrees=lon)
     observer = geographic.at(t)
-    pos = observer.from_altaz(alt_degrees=90, az_degrees=0)
+    pos = observer.from_altaz(alt_degrees=Angle.ZENITH, az_degrees=Angle.HORIZON)
     ra, dec, distance = pos.radec()
     return ra, dec
+
 
 '''
 converte horas em valor entre 0-100.
 '''
-def convert_radec_to_centesimal(ra,dec,image_width,image_height):
-    y = round(((image_height/2) * dec)/90,0) # 90 graus
-    if(y < 0):
-        #longitude, valor angulo 0 é no centro da imagem. polo sul é negativo, polo norte positivo.
-        y += image_height/2 + abs(y)
-    x = round((image_width * (24 - ra))/24,0) #24 horas
+
+
+def convert_radec_to_centesimal(ra, dec, image_width, image_height):
+    y = round(((image_height / 2) * dec) / Angle.ZENITH, 0)  # 90 graus
+    if (y < 0):
+        # longitude, valor angulo 0 é no centro da imagem. polo sul é negativo, polo norte positivo.
+        y += image_height / 2 + abs(y)
+    x = round((image_width * (24 - ra)) / 24, 0)  # 24 horas
     # print('y:{}, x:{}'.format(y,x))
-    return x,y
+    return x, y
 
 
 def main():
     print("hello")
     eph = api.load('de421.bsp')
     img = Image.open("./Resources/Original_Images/starmap.png")
-    w,h = img.size
-    ra,dec = get_ra_dec_above_observer(2021,8,20,20,10, -41.00106,-20.37511)
-    print('ra:{}, dec:{}'.format(ra,dec))
+    w, h = img.size
+    # ra,dec = get_ra_dec_above_observer(2021,8,20,20,10, -41.00106,-20.37511)
+    ra, dec = get_ra_dec_above_observer(2023, 2, 1, 22, 10, -41.00106, -20.37511)
+    print('ra:{}, dec:{}'.format(ra, dec))
     ra_float = float('{:.8f}'.format(ra.hours))
     dec_float = float('{:+.8f}'.format(dec.degrees))
     print('ra:{}, dec:{}'.format(ra_float, dec_float))
     ts = api.load.timescale()
-    skytime = ts.utc(2021,8,20,20,10)
-    moon_phase = almanac.moon_phase(eph,skytime)
+    skytime = ts.utc(2021, 8, 20, 20, 10)
+    moon_phase = almanac.moon_phase(eph, skytime)
     print("moonphase: {}".format(moon_phase.degrees))
-    sky_x, sky_y = convert_radec_to_centesimal(ra_float,dec_float,w,h)
+    sky_x, sky_y = convert_radec_to_centesimal(ra_float, dec_float, w, h)
     im_square = crop_center(img, sky_x, sky_y, 700, 700)
     im_thumb = mask_circle_solid(im_square)
     im_thumb.show()
     # im_square.show()
+
 
 if __name__ == '__main__':
     main()
